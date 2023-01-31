60cbe00e591a205120c5fcd24cba3cf2a6b9acf8
Leyla Akin <lakin@smith.edu>
2023-01-31 14:27:43 -0500

    fixed merge error

--- octocat_count.R
+++ octocat_count.R
@@ -1,9 +1,5 @@
 # to count the lines in octocat.txt
-<<<<<<< HEAD
-#uh oh
-=======
 #Conflict
->>>>>>> d3af14fbc805e2a6a6643ea201d36898bf3b7953
 octocat = readLines("./octocat.txt")
 
 length(octocat)
